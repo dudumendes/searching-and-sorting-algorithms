43098f9e0a78f9b1fd8f7f20ebf6108efaf964bf
Eduardo Mendes <eduardomendes@Eduardos-MacBook-Pro.local>
2019-08-28 10:47:52 -0300

    selection sort variations

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 import { optimunBubbleSort } from './sorting/bubbleSort'
-import { selectionSort } from './sorting/selectionSort'
+import { selectionSortMax2 } from './sorting/selectionSort'
 import { insertionSort } from './sorting/insertionSort'
 
-let numbers = [13, 8, 7, 6, 2, 1], numbers2 = [13, 8, 7, 6, 2, 1], numbers3 = [13, 8, 7, 6, 2, 1]
+let numbers = [9, 11, 10, 16, 7, 5, 2], numbers2 = [13, 8, 7, 6, 2, 1], numbers3 = [13, 8, 7, 6, 2, 1]
 
-insertionSort(numbers3)
-console.log(numbers3)
+// insertionSort(numbers3)
+// console.log(numbers3)
 
-selectionSort(numbers)
-console.log(numbers)
+selectionSortMax2(numbers)
+// console.log(numbers)
 
-optimunBubbleSort(numbers2)
-console.log(numbers2)
+// optimunBubbleSort(numbers2)
+// console.log(numbers2)
--- src/sorting/selectionSort.js
+++ src/sorting/selectionSort.js
@@ -17,7 +17,60 @@ export function selectionSort(vector) {
             [vector[i], vector[minimum]] = [vector[minimum], vector[i]]
             swapTotal++
         }
+        console.log(vector)
     }
     console.log(`swapTotal: ${swapTotal}, comparisons: ${comparisonTotal}`)
 
 }
+
+
+export function selectionSortMax(vector) {
+    let swapTotal = 0;
+    let comparisonTotal = 0;
+
+    for (let i = 0; i < vector.length - 1; i++) {
+        let maximum = vector.length - 1 - i
+        
+        for (let j = 0; j < vector.length - 2 - i; j++) {
+            comparisonTotal++
+
+            if (vector[j] > vector[maximum]) {
+                maximum = j
+            }
+        }
+
+        if (maximum != vector.length - 1 - i) {
+            [vector[vector.length - 1 - i], vector[maximum]] =
+                [vector[maximum], vector[vector.length - 1 - i]]
+            swapTotal++
+        }
+        console.log(vector)
+    }
+    console.log(`swapTotal: ${swapTotal}, comparisons: ${comparisonTotal}`)
+}
+
+export function selectionSortMax2(vector) {
+    let swapTotal = 0;
+    let comparisonTotal = 0;
+
+    for (let i = vector.length - 1; i > 0; i--) {
+        let maximum = i
+        
+        for (let j = i - 1; j >= 0; j--) {
+            comparisonTotal++
+
+            if (vector[j] > vector[maximum]) {
+                maximum = j
+            }
+        }
+
+        if (maximum != i) {
+            [vector[i], vector[maximum]] =[vector[maximum], vector[i]]
+            swapTotal++
+        }
+        console.log(vector)
+    }
+    console.log(`swapTotal: ${swapTotal}, comparisons: ${comparisonTotal}`)
+
+}
+
