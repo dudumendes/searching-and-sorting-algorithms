8567c55eeb6a708c04c1eacfe666c883e8619ed7
Eduardo Mendes <eduardomendes@Eduardos-MacBook-Pro.local>
2019-09-23 10:39:23 -0300

    printing vector on each loop

--- src/sorting/countingSort.js
+++ src/sorting/countingSort.js
@@ -1,24 +1,27 @@
 export function countingSort(vector, max) {
     let counter = []
-    let ordered = []
+    let sorted = []
 
     //First step
     for(let i = 0; i <= max; i++) {
         counter[i] = 0
     }
+    console.log(`1 - counter: ${counter}`)
 
     for(let i = 0; i < vector.length; i++) {
         counter[vector[i]] += 1
     }
+    console.log(`2 - counter: ${counter}`)
 
     for(let i = 1; i < counter.length; i++) {
         counter[i] += counter[i - 1]
     }
+    console.log(`3 - counter: ${counter}`)
 
     for(let i = 0; i < vector.length; i++) {
-       ordered[counter[vector[i]] - 1] = vector[i]
+       sorted[counter[vector[i]] - 1] = vector[i]
        counter[vector[i]] -= 1
     }
 
-    return ordered
+    return sorted
 }
