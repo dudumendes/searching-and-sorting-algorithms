dc82f5eecdc06d6a3a9afdaaf1354982ca664fe2
Eduardo Mendes <eduardomendes@Eduardos-MacBook-Pro.local>
2019-09-30 15:13:06 -0300

    merge sort example

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import { mergeSort } from './sorting/mergeSort'
 
-let numbers = [1, 4, 1, 2, 7, 5, 2], numbers2 = [13, 8, 7, 6, 2, 1], numbers3 = [13, 8, 7, 6, 2, 1]
+let numbers = [1, 4, 1, 2, 7, 5, 2, 3], numbers2 = [13, 8, 7, 6, 2, 1], numbers3 = [13, 8, 7, 6, 2, 1]
 
 console.log(numbers)
-mergeSort(numbers, numbers[0], numbers[numbers.length-1])
+mergeSort(numbers, 0, 7)
 console.log(numbers)
