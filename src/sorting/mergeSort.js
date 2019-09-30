3ee085f5a3a5685d0be9d588eb58cea80054758b
Eduardo Mendes <eduardomendes@Eduardos-MacBook-Pro.local>
2019-09-30 10:55:25 -0300

    merge Sort with bugs

--- src/sorting/mergeSort.js
+++ src/sorting/mergeSort.js
@@ -1,6 +1,6 @@
 export function mergeSort(vector, start, end) {
     if (start < end) {
-        let middle = Math.floor( (start + end) / 2 )
+        let middle = Math.floor((start + end) / 2)
         mergeSort(vector, start, middle)
         mergeSort(vector, middle + 1, end)
         merge(vector, start, middle, end)
@@ -8,32 +8,32 @@ export function mergeSort(vector, start, end) {
 }
 
 function merge(vector, start, middle, end) {
+    let size = end - start + 1
     let sorted = []
     let left = start
     let right = middle + 1
     let actual = start
-    
-    while(!endLeft || !endRight) {
+    let endLeft = false, endRight = false
+
+    while (!endLeft || !endRight) {
         if (!endLeft && !endRight) {
             if (vector[left] < vector[right]) {
                 sorted[actual++] = vector[left++]
             } else {
                 sorted[actual++] = vector[right++]
             }
+
+        } else if (endLeft) {
+            sorted[actual++] = vector[right++]
+        } else {
+            sorted[actual++] = vector[left++]
         }
 
-        if (endLeft) {
-            while(!endRight) {
-                sorted[actual++] = vector[right++]
-            }
-        }
+        endLeft = left > middle ? true : false
+        endRight = right > end ? true : false
+    }
 
-        if (endRight) {
-            while(!endLeft) {
-                sorted[actual++] = vector[left++]
-            }
-        }
-
-        //sorted[actual++] = (vector[left] < vector[right]) ? vector[left] : vector[right]
+    for (let i = 0; i < size; i++) {
+        vector[start + i] = sorted[i]
     }
 }
