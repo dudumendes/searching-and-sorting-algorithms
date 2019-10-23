731ea8371e3fb621d0a64a791f919cfe6f195640
Eduardo Mendes <eduardomendes@Eduardos-MacBook-Pro.local>
2019-10-23 09:43:16 -0300

    Quick Sort example

---
+++ src/app.8.js
@@ -0,0 +1,7 @@
+import { quickSort } from './sorting/quickSort'
+
+let numbers = [1, 4, 1, 2, 7, 5, 2, 3], numbers2 = [13, 8, 7, 6, 2, 1], numbers3 = [13, 8, 7, 6, 2, 1]
+
+console.log(numbers)
+quickSort(numbers, 0, 7)
+console.log(numbers)
