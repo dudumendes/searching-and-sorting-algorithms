7ce189c51d63442b209237739b8fb8ead21fae34
Eduardo Mendes <eduardomendes@Eduardos-MacBook-Pro.local>
2019-10-23 10:51:12 -0300

    Heap example

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
-import { heapSort } from './sorting/heapSort'
+import Element from './searching/Element'
+import Heap from './searching/Heap'
 
-let numbers = [2, 8, 5, 3, 9, 1]
+let element = new Element(10)
+element.id = 30
+console.log(element.id)
+console.log(element)
 
-console.log(numbers)
-heapSort(numbers)
-console.log(numbers)
+let numbers = [8, 7, 3, 10]
+
+let heap = new Heap(5)
+
+numbers.forEach( key => heap.add(new Element(key)))
+
+console.log(heap._elements)
