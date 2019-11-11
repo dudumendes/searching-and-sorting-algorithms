7b1ae767bbd70292066083f00341419c625b86e8
Eduardo Mendes <eduardomendes@Eduardos-MacBook-Pro.local>
2019-11-11 11:06:26 -0300

    examples updated

--- src/app.11.js
+++ src/app.11.js
@@ -1,9 +1,9 @@
 import { quickSort } from './sorting/quickSort'
 import { createRandomArray, createSortedArrayDesc, createSortedArrayAsc } from './util/array'
 
-let numbers = createRandomArray(7000)
-let sortedNumbersAsc = createSortedArrayAsc(7000)
-let sortedNumbersDesc = createSortedArrayDesc(7000)
+let numbers = createRandomArray(10000)
+let sortedNumbersAsc = createSortedArrayAsc(10000)
+let sortedNumbersDesc = createSortedArrayDesc(10000)
 
 console.time('quickSort random');
 quickSort(numbers, 0, numbers.length - 1)
--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
 import Element from './searching/Element'
 import Heap from './searching/Heap'
 
-let element = new Element(10)
-element.id = 30
-console.log(element.id)
-console.log(element)
+let numbers = [100, 42, 50, 41, 39, 48, 43]
 
-let numbers = [8, 7, 3, 10]
-
-let heap = new Heap(5)
+let heap = new Heap(7)
 
 numbers.forEach( key => heap.add(new Element(key)))
 
+console.log(heap._elements)
+let max = heap.remove()
+console.log(`max: ${max}`)
+console.log(heap._elements)
+
+max = heap.remove()
+console.log(`max: ${max}`)
 console.log(heap._elements)
